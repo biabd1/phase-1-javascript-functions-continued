d55c9c9fc8266eedf2c2aada0a593ef61583d970
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-16 04:11:05 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+function saturdayFun(fun= 'roller-skate'){
+    return `This Saturday, I want to ${fun}!`
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(work='go to the office'){
+    return  `This Monday, I will ${work}.`
+}
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(wrap='*') {
+    const result = function emphatic(adjective) {
+      return `You are ${wrap}${adjective}${wrap}!`; 
+    };
+    return result; 
+  }
+  emphatic('a hard worker')
